f053a787637fded62038ec49ec0f7e5e7a4224c7
Tom Schneider <ric0.github@gmail.com>
2022-10-30 14:41:09 +0100

    feat: implement button interactions
    - add SpecialButtons (miss, bull, bullseye)
    - extract components
    - use object destructuring
    - add state to track the button inputs
    - display button interactions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import { Button, makeStyles, Text, tokens } from '@fluentui/react-components';
-import { useState } from 'react';
+import {
+  Button,
+  makeStyles,
+  Text,
+  Title3,
+  tokens,
+} from '@fluentui/react-components';
+import { useReducer, useState } from 'react';
 import './App.css';
 
 const useColorOverrides = makeStyles({
   1: { backgroundColor: tokens.colorPaletteCornflowerBackground2 },
   2: { backgroundColor: tokens.colorPaletteLavenderBackground2 },
   3: { backgroundColor: tokens.colorPaletteGrapeBackground2 },
+  bull: { backgroundColor: tokens.colorPalettePinkBackground2 },
+  bullseye: { backgroundColor: tokens.colorPaletteMagentaBackground2 },
+  miss: { backgroundColor: tokens.colorPaletteBeigeBackground2 },
 });
 
-function MultiplierButtons(props) {
+function SpecialButtons({ addValueWithMultiplier }) {
+  const colorOverrides = useColorOverrides();
+
+  return (
+    <div>
+      <Button
+        shape="circular"
+        appearance="primary"
+        className={colorOverrides.miss}
+        onClick={() => addValueWithMultiplier(0, 1)}
+      >
+        MISS
+      </Button>
+      <Button
+        shape="circular"
+        appearance="primary"
+        className={colorOverrides.bull}
+        onClick={() => addValueWithMultiplier(25, 1)}
+      >
+        BULL
+      </Button>
+      <Button
+        shape="circular"
+        appearance="primary"
+        className={colorOverrides.bullseye}
+        onClick={() => addValueWithMultiplier(25, 2)}
+      >
+        BULLSEYE
+      </Button>
+    </div>
+  );
+}
+
+function MultiplierButtons({ multiplier, setMultiplier }) {
   const colorOverrides = useColorOverrides();
 
   return (
@@ -16,54 +58,139 @@ function MultiplierButtons(props) {
       <Button
         appearance="primary"
         className={colorOverrides[1]}
-        onClick={() => props.setMultiplier(1)}
+        onClick={() => setMultiplier(1)}
       >
         Single
       </Button>
       <Button
         appearance="primary"
         className={colorOverrides[2]}
-        onClick={() => props.setMultiplier(2)}
+        onClick={() => setMultiplier(2)}
       >
         Double
       </Button>
       <Button
         appearance="primary"
         className={colorOverrides[3]}
-        onClick={() => props.setMultiplier(3)}
+        onClick={() => setMultiplier(3)}
       >
         Triple
       </Button>
-      <Text>The current multiplier is {props.multiplier}</Text>
+      <Text>The current multiplier is {multiplier}</Text>
     </div>
   );
 }
 
-function InputButtons() {
+function TwentyButtons({ multiplier, addValue }) {
   const colorOverrides = useColorOverrides();
-  const [multiplier, setMultiplier] = useState(1);
 
+  /**
+   * @type {number[]}
+   */
   const numberArray = Array.from({ length: 20 }, (v, k) => k + 1);
-  const twentyButtons = numberArray.map((number, idx) => (
-    <Button
-      className={colorOverrides[multiplier]}
-      shape="circular"
-      appearance="primary"
-      key={idx}
-    >
-      {number}
-    </Button>
-  ));
+
+  return (
+    <div>
+      {numberArray.map((number, idx) => (
+        <Button
+          className={colorOverrides[multiplier]}
+          shape="circular"
+          appearance="primary"
+          key={idx}
+          onClick={() => addValue(number)}
+        >
+          {number}
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+function throwsReducer(state, action) {
+  // TODO use `use-immer-reducer` to simplify the state patching
+  // TODO see: https://beta.reactjs.org/learn/extracting-state-logic-into-a-reducer#writing-concise-reducers-with-immer
+  switch (action.type) {
+    case 'addValueWithMultiplier': {
+      return [
+        ...state,
+        {
+          value: action.value,
+          multiplier: action.multiplier,
+        },
+      ];
+    }
+    default:
+      return { ...state };
+  }
+}
+
+function InputButtons() {
+  const [multiplier, setMultiplier] = useState(1);
+  const [throws, dispatch] = useReducer(throwsReducer, []);
+
+  /**
+   * the multiplier is provided by the MultiplierButtons as a state (hook)
+   * @param value
+   */
+  function handleValueWithMultiplier(value) {
+    dispatch({
+      type: 'addValueWithMultiplier',
+      value,
+      multiplier,
+    });
+  }
+
+  function handleValueWithExplicitMultiplier(value, multiplier) {
+    dispatch({
+      type: 'addValueWithMultiplier',
+      value,
+      multiplier,
+    });
+  }
 
   return (
     <div>
       <p>Input Buttons</p>
 
-      <MultiplierButtons
-        setMultiplier={setMultiplier}
-        multiplier={multiplier}
+      <SpecialButtons
+        addValueWithMultiplier={handleValueWithExplicitMultiplier}
       />
-      <div>{twentyButtons}</div>
+
+      <MultiplierButtons
+        multiplier={multiplier}
+        setMultiplier={setMultiplier}
+      />
+
+      <TwentyButtons
+        multiplier={multiplier}
+        addValue={handleValueWithMultiplier}
+      />
+
+      {throws.map((item, idx) => (
+        <ThrowOutput item={item} key={idx} />
+      ))}
+    </div>
+  );
+}
+
+function ThrowOutput({ item }) {
+  const { value, multiplier } = item;
+  if (isNaN(value) || isNaN(multiplier)) {
+    console.error(
+      'received invalid data, either value or multiplier are NaN',
+      item
+    );
+    return;
+  }
+
+  const computedValue = value * multiplier;
+
+  return (
+    <div>
+      <Text>{computedValue}</Text>
+      <Text size={100}>
+        ={value} x{multiplier}
+      </Text>
     </div>
   );
 }
@@ -71,7 +198,7 @@ function InputButtons() {
 function App() {
   return (
     <div>
-      <Text>Hello World</Text>
+      <Title3>Dart Board</Title3>
       <InputButtons />
     </div>
   );
